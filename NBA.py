3179ba7ce121120611c780e9e8d203b2636446ac
darderrdur17 <e0960472@u.nus.edu>
2023-11-13 02:40:22 +0800

    modified:   NBA.py

--- NBA.py
+++ NBA.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import base64
-import plotly
 import plotly.express as px
-
 
 st.title('National Basketball Association[NBA] Player Stats Explorer')
 
@@ -31,16 +29,16 @@ selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
 st.header('Display Player Stats of Selected Team(s)')
-st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.write(f'Data Dimension: {df_selected_team.shape[0]} rows and {df_selected_team.shape[1]} columns.')
 st.dataframe(df_selected_team)
 
-def filedownload(df):
+def file_download(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
     return href
 
-st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+st.markdown(file_download(df_selected_team), unsafe_allow_html=True)
 
 if st.button('Intercorrelation Heatmap'):
     st.header('Intercorrelation Matrix Heatmap')
@@ -48,4 +46,3 @@ if st.button('Intercorrelation Heatmap'):
     fig = px.imshow(corr, x=corr.index, y=corr.columns, color_continuous_scale='Viridis', labels=dict(color='Correlation'))
     fig.update_layout(title_text='Intercorrelation Matrix Heatmap')
     st.plotly_chart(fig)
-
