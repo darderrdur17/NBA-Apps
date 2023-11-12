2bd30736e46931f660c915299f7a1b39537a1c3c
darderrdur17 <e0960472@u.nus.edu>
2023-11-13 02:29:24 +0800

    Change of structure

--- NBA.py
+++ NBA.py
@@ -1,21 +1,16 @@
-import requests
-import pandas as pd
-import base64
-import matplotlib.pyplot as plt
-import requests
-import pandas as pd
-import base64
-import numpy as np
 import streamlit as st
+import pandas as pd
+import base64
 import plotly.express as px
 
-st.title("National Basketball Association[NBA] Player Stats Explorer")
+st.title('National Basketball Association[NBA] Player Stats Explorer')
+
 st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year:', list(reversed(range(1950, 2022))))
 
 @st.cache
 def load_data(year):
-    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
+    url = f"https://www.basketball-reference.com/leagues/NBA_{year}_per_game.html"
     html = pd.read_html(url, header=0)
     df = html[0]
     raw = df.drop(df[df.Age == 'Age'].index)
@@ -23,41 +18,32 @@ def load_data(year):
     playerstats = raw.drop(['Rk'], axis=1)
     return playerstats
 
-def main():
-    selected_year = st.sidebar.slider('Select Year', 2000, 2023, 2022)
-    playerstats = load_data(selected_year)
+playerstats = load_data(selected_year)
 
-    sorted_unique_team = sorted(playerstats.Tm.unique())
-    selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+sorted_unique_team = sorted(playerstats.Tm.unique())
+selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
 
-    unique_pos = ['C', 'PF', 'SF', 'PG', 'SG']
-    selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
+unique_pos = ['C', 'PF', 'SF', 'PG', 'SG']
+selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 
-    df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
+df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
-    st.header('Display Player Stats of Selected Team(s)')
-    st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.header('Display Player Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.dataframe(df_selected_team)
 
-    fig = px.scatter(
-        df_selected_team,
-        x='PTS', y='TRB',
-        color='Pos',
-        size='Age',
-        hover_name='Player',
-        title='NBA Player Stats',
-        labels={'PTS': 'Points per Game', 'TRB': 'Total Rebounds per Game'},
-        template='plotly_dark'
-    )
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Heatmap')
+    corr = df_selected_team.corr()
+    fig = px.imshow(corr, x=corr.index, y=corr.columns, color_continuous_scale='Viridis', labels=dict(color='Correlation'))
+    fig.update_layout(title_text='Intercorrelation Matrix Heatmap')
     st.plotly_chart(fig)
-
-    # Download CSV file link
-    st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
-
-    # Intercorrelation Heatmap
-    if st.button('Intercorrelation Heatmap'):
-        st.header('Intercorrelation Matrix Heatmap')
-        correlation_heatmap(df_selected_team)
-
-if __name__ == "__main__":
-    main()
 
